69e5d8bee3bcbb5d84030dcd170e328f46cc7929
basile <basile.desjuzeur@hec.edu>
2025-09-19 15:56:56 +0200

    [FIX] data was in gitignore

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from choose_museum import main as choose_museum
 
 
 museum = choose_museum()
-st.write(f"Musée choisi : {museum['nom_officiel_du_musee']}")
+st.write(f"Musée choisi : {museum['nom_officiel_du_musee'].values[0]}")
 st.map(museum)
